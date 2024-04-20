f9e66d62e918454b2febe31a80cb77db9178cfce
Sumit Yadav <sumityadav329@gmail.com>
2024-04-20 21:27:02 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# CVproject-object-detection-yolo
+<!-- End-to-end-Object-Detection-Project -->
+
+<!-- How to run: -->
+```bash 
+conda create -p .venv python=3.7 -y
+```
+```bash
+conda activate .venv 
+```
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'sumityadav329',
+    author_email= 'sumityadav329@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
